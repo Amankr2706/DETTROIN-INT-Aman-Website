2226c71d084e7e5ec45ddceda39b711e8758798c
Amankr2706 <amankumarraman27@gmail.com>
2026-07-26 00:08:28 +0530

    Initial project setup with Tailwind and React Router

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-600">
+      <h1 className="text-5xl font-bold text-white">
+        Dettroin School Website
+      </h1>
+    </div>
+  );
+}
+export default App;
